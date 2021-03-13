e03aa37fbc0dc9579e57bb2e2ed6c3ea710bc959
Luta Krystal <nguyenvanthai305@gmail.com>
2021-03-13 15:36:32 +0700

    middleware

--- middleware/verify.middleware.js
+++ middleware/verify.middleware.js
@@ -1,23 +1,19 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user.model');
 
-module.exports = async (request, response, next) => {
-    console.log(request)
-    // send to client token -> post (req.body verify)
-    const token = request.header('auth-token');
-
-    
-    if (!token) return response.status(401).send('Access Denied');
-
-    try {
-        const verified = jwt.verify(token, 'shhh');
-        const user = await User.findOne({_id: verified._id});
-        if (user) {
-            next();
-        } else {
-            console.log(false)
-        }
-    } catch (err) {
-        return response.status(400).send('Invalid Token');
+module.exports.verify = async function (req, res, next) {
+    console.log(req.headers);
+    const token = req.headers.authorization;
+    if (!token) {
+      return;
+    } else {
+      const verified = jwt.verify(token, "shhh");
+      const user = await User.findOne({_id: verified._id});
+      if (user) {
+        next();
+      } else {
+        console.log(false);
+        return;
+      }
     }
-};
+  };
