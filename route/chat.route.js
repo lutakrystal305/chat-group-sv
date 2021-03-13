59233d4e044ba17b19bd792ad7ee07e66427e3ba
Luta Krystal <nguyenvanthai305@gmail.com>
2021-03-13 15:52:27 +0700

    fix-middleware

--- route/chat.route.js
+++ route/chat.route.js
@@ -2,16 +2,16 @@ const express = require("express");
 const router = express.Router();
 const controller1 = require("../controller/message.controller");
 const controller2 = require('../controller/room.controller');
-const verify = require("../middleware/verify.middleware");
+const middleware = require("../middleware/verify.middleware");
 
-router.post('/message', verify, controller1.message);
+router.post('/message', middleware.verify, controller1.message);
 //router.post('/topMess', controller1.topMess);
-router.post('/upMess', verify, controller1.upMess);
-router.get('/getRooms', verify, controller2.getRooms);
-router.post('/findRoom', verify, controller2.findRoom);
-router.post('/createRoom', verify, controller2.createRoom);
-router.post('/checkRoom', verify, controller2.checkRoom);
-router.post('/getRoom', verify,controller2.getRoom);
-router.post('/leaveRoom', verify, controller2.leaveRoom);
+router.post('/upMess', middleware.verify, controller1.upMess);
+router.get('/getRooms', middleware.verify, controller2.getRooms);
+router.post('/findRoom', middleware.verify, controller2.findRoom);
+router.post('/createRoom', middleware.verify, controller2.createRoom);
+router.post('/checkRoom', middleware.verify, controller2.checkRoom);
+router.post('/getRoom', middleware.verify, controller2.getRoom);
+router.post('/leaveRoom', middleware.verify, controller2.leaveRoom);
 
 module.exports = router;
--- route/user.route.js
+++ route/user.route.js
@@ -6,7 +6,7 @@ var verify = require("../middleware/verify.middleware");
 router.post('/login', controller.login);
 router.post('/check', controller.check);
 router.post('/create', controller.create);
-router.post('/getMember', verify, controller.getMember);
+router.post('/getMember', verify.verify, controller.getMember);
 router.post('/loginFB', controller.loginFB);
 //router.post('/getRoom', controller.getRoom);
 
